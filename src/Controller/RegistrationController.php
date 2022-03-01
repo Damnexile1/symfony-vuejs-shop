0f2ef789f3b08632dc0c9be0783fe05c1f241579
damnexile <tbrikulya@accurateweb.ru>
2022-03-01 23:10:21 +0500

    +Регистрация пользователя +flash сообщение

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -88,12 +88,12 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('homepage');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('homepage');
     }
 }
